1d85dc99a57a6211efa9bdb98195476d41265a79
Raneem Khafagy <69002626+Raneem-Khafagy@users.noreply.github.com>
2021-04-29 11:29:55 +0200

    added work manager

--- technews/lib/main.dart
+++ technews/lib/main.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:technews/utils/services/rest_api_service.dart';
-import 'package:technews/views/components/splash_screen.dart';
+import 'utils/services/rest_api_service.dart';
+import 'views/components/splash_screen.dart';
+import 'constants/api_path.dart';
 import 'views/screens/home.dart';
 import 'utils/themes/theme_config.dart';
+import 'package:workmanager/workmanager.dart';
+
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+
+    print("Native called background task");
+    // the update news response will be emitted here
+
+    News().fetchData(UrlProvider().geturl());
+
+
+    return Future.value(true);
+  });
+}
+
+
 void main() {
-  fetchData();
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setEnabledSystemUIOverlays(
-  //     [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(
+    callbackDispatcher, // The top level function, aka callbackDispatcher
+    isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+  );
+  Workmanager().registerPeriodicTask("1", "Apiservice", frequency: Duration(minutes: 15), );
+
   runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider<ThemeNotifier>(create: (_) => ThemeNotifier()),
-      ],
-      child: MyApp(),
-    ),);
+    providers: [
+      ChangeNotifierProvider < ThemeNotifier > (create: (_) => ThemeNotifier()),
+      ChangeNotifierProvider < News > (create: (_) => News()),
+      ChangeNotifierProvider < UrlProvider > (create: (_) => UrlProvider()),
+    ],
+    child: MyApp(),
+  ), );
 }
 class MyApp extends StatelessWidget {
- @override
+  @override
   Widget build(BuildContext context) {
-    final themeNotifier = Provider.of<ThemeNotifier>(context);
-
+    final themeNotifier = Provider.of < ThemeNotifier > (context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'News App',
@@ -32,4 +54,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
- 
